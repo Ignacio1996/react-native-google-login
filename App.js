94a69f35129d349de9e50f225d92d98c7c2edb81
Nicolas Aguirre <in-aguirre@hotmail.com>
2019-11-08 09:05:55 -0500

    clean app setup

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Screens
 import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
-
-
 
 //React Navigation Setup
 import { createStackNavigator } from 'react-navigation-stack';
@@ -19,11 +18,3 @@ const App = createAppContainer(MainNavigator);
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

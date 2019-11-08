6e81858a45075010c3119d78fd2f7accffaabf28
Nicolas Aguirre <in-aguirre@hotmail.com>
2019-11-08 09:23:00 -0500

    nothing working

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native'
+import { Text, View, StyleSheet, Button } from 'react-native';
 
-export default class HomeScreen extends Component {
+// IOS client ID 978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com
+
+const IOS_CLIENT_ID = '978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com';
+const ANDROID_CLIENT_ID = ''
+
+export default class LoginScreen extends Component {
   render() {
     return (
       <View style={styles.container}>

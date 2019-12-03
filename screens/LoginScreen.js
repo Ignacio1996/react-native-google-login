6fd4cc9415beceb0d4d457ad179b7ad5aace53a2
Nicolas Aguirre <in-aguirre@hotmail.com>
2019-12-03 09:59:09 -0500

    working with profile name

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,41 +3,38 @@ import { Text, View, StyleSheet, Button } from "react-native";
 
 import * as Google from "expo-google-app-auth";
 
-// IOS client ID 978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com
-
 const IOS_CLIENT_ID =
   "978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com";
-const ANDROID_CLIENT_ID = "978708541571-qfe9eks6kkf2a9nf94tlrhe7qsv7no1n.apps.googleusercontent.com";
-
+const ANDROID_CLIENT_ID =
+  "978708541571-qfe9eks6kkf2a9nf94tlrhe7qsv7no1n.apps.googleusercontent.com";
 
 export default class LoginScreen extends Component {
   signInWithGoogle = async () => {
-      try {
-        const result = await Google.logInAsync({
-          iosClientId: IOS_CLIENT_ID,
-          androidClientId: ANDROID_CLIENT_ID,
-          scopes: ["profile", "email"]
-        });
+    try {
+      const result = await Google.logInAsync({
+        iosClientId: IOS_CLIENT_ID,
+        androidClientId: ANDROID_CLIENT_ID,
+        scopes: ["profile", "email"]
+      });
 
-        if (result.type === "success") {
-          this.props.navigation.navigate("Profile"); //after Google login redirect to Profile
-          return result.accessToken;
-        } else {
-          return { cancelled: true };
-        }
-      } catch (e) {
-        return { error: true };
+      if (result.type === "success") {
+        console.log("LoginScreen.js.js 22 | ", result.user.givenName);
+        this.props.navigation.navigate("Profile", {
+          username: result.user.givenName
+        }); //after Google login redirect to Profile
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
       }
-
+    } catch (e) {
+      return { error: true };
+    }
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Button
-          title="Login with Google"
-          onPress={this.signInWithGoogle}
-        />
+        <Button title="Login with Google" onPress={this.signInWithGoogle} />
       </View>
     );
   }
--- screens/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import LoginScreen from './LoginScreen';
-import ProfileScreen from './ProfileScreen';
-
-export {
-  HomeScreen,
-  LoginScreen,
-}

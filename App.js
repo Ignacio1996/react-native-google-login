5faee7c2b111b7529496c71c976655f794af97b1
Nicolas Aguirre <in-aguirre@hotmail.com>
2019-12-03 09:49:05 -0500

    working login ios and android

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
 // Screens
 import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
 
 //React Navigation Setup
-import { createStackNavigator } from 'react-navigation-stack';
 import {  createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 const MainNavigator = createSwitchNavigator({
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+
+import * as Google from "expo-google-app-auth";
 
 // IOS client ID 978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com
 
-const IOS_CLIENT_ID = '978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com';
-const ANDROID_CLIENT_ID = ''
+const IOS_CLIENT_ID =
+  "978708541571-md66h7bbmmom8vmusp1qo7adojckfhab.apps.googleusercontent.com";
+const ANDROID_CLIENT_ID = "978708541571-qfe9eks6kkf2a9nf94tlrhe7qsv7no1n.apps.googleusercontent.com";
+
 
 export default class LoginScreen extends Component {
+  signInWithGoogle = async () => {
+      try {
+        const result = await Google.logInAsync({
+          iosClientId: IOS_CLIENT_ID,
+          androidClientId: ANDROID_CLIENT_ID,
+          scopes: ["profile", "email"]
+        });
+
+        if (result.type === "success") {
+          this.props.navigation.navigate("Profile"); //after Google login redirect to Profile
+          return result.accessToken;
+        } else {
+          return { cancelled: true };
+        }
+      } catch (e) {
+        return { error: true };
+      }
+
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Button
           title="Login with Google"
-          onPress={()=>this.props.navigation.navigate("Profile")}
+          onPress={this.signInWithGoogle}
         />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
